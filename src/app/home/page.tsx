d60bbb72976c0332f93ffd86c233c89d5b94ef8c
Jahanzeb <jahanzebsoomro543@gmail.com>
2024-09-19 23:00:44 +0500

    firebase authentication

---
+++ src/app/home/page.tsx
@@ -0,0 +1,22 @@
+
+"use client";
+
+import { saveTodo } from "@/src/firebase/firebasefirestore";
+import { useState } from "react"
+
+
+export default function Home() {
+    const [todo, setTodo] = useState('')
+    return (
+        <>
+            <h1>Hello Home</h1>
+            <input type="text"
+                value={todo}
+                onChange={(e) => { setTodo(e.target.value) }}
+            />
+            <button onClick={() => {
+                saveTodo(todo)
+            }}>Add New Todo</button>
+        </>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { loginWithEmailPassword, signupWithEmailPassword } from "@/src/firebase/firebaseauth";
+import { useState } from "react";
+
+
+export default function Home() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [rollNum, setRollNum] = useState("");
+  const [studentName, setStudentName] = useState("");
+
+
+  // const auth = getAuth(app);
+
+  // const signup = async () => {
+  //   try{
+  //     let userCredential = await createUserWithEmailAndPassword(auth, email, password);
+  //   }catch(e){
+  //     console.log(e)
+  //   }
+
+
+  // }
+
+  return (
+    <>
+      <h1>Hello Auth</h1>
+      Roll Number:   <input type="email" style={{border: "1px solid black"}}
+        value={rollNum}
+        onChange={(e) => { setRollNum(e.target.value) }}
+      /><br />
+
+      Student Name:   <input type="email"
+        value={studentName}
+        onChange={(e) => { setStudentName(e.target.value) }}
+      /><br />
+
+
+      Email:   <input type="email"
+        value={email}
+        onChange={(e) => { setEmail(e.target.value) }}
+      /><br />
+
+      Password:   <input type="password"
+        value={password}
+        onChange={(e) => { setPassword(e.target.value) }}
+      />
+
+      <button onClick={
+        () => { signupWithEmailPassword(email, password, rollNum, studentName) }
+
+        // signup
+
+      }
+      >
+        Signup
+      </button>
+
+      <button onClick={
+        () => { loginWithEmailPassword(email, password) 
+        }}
+      >
+        Login
+      </button>
+
+    </>
+  );
+}
